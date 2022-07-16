d0ff14074248d1c2b6f15a5498afb0257bea9b87
Wallace Valeriano Ignacio Bulacios <RH85911@crer.local>
2022-07-16 16:56:27 -0300

    Atualização repo e aula 18

---
+++ Aula-18/app.js
@@ -0,0 +1,16 @@
+// Booleans e comporações
+
+
+// Métodos que pode retornar booleans
+const email = 'brucewayne@rogermelo.com.br'
+
+    // metodo include verifica de um determinado caractere existe dentro da variavel informada
+    const includes = email.includes('@') // se @ existir na strig informadqa retorna true
+    console.log(includes)
+
+    const names = ['dio', 'roger', 'robert']
+    const namesIncludes = names.includes('roger')
+    console.log(namesIncludes)
+
+
+// Operadores de Comparação
--- repo-curso/curso-de-js-roger-melo/exercicio-03/app.js
+++ repo-curso/curso-de-js-roger-melo/exercicio-03/app.js
@@ -10,7 +10,7 @@
 02 - Armazene um array vazio em uma let "brazilianFoods";
 */
 
-
+let brazilianFoods = []
 
 /*
 03 - Use o método adequado para adicionar os 3 seguintes itens ao array:
@@ -22,6 +22,9 @@
 - Exiba a brazilianFoods no console.
 */
 
+brazilianFoods.push('Coxinha', null, 'Brigadeiro')
+
+console.log(brazilianFoods)
 
 
 /*
@@ -32,7 +35,10 @@ array "brazilianFoods" (null) seja substituído por "Pão de Queijo";
 - Exiba apenas o 2º item desse array no console.
 */
 
+brazilianFoods[1] = 'Pão de Queijo'
 
+console.log(brazilianFoods)
+console.log(brazilianFoods[1])
 
 /*
 05 - Comente o console.log acima e:
@@ -47,7 +53,9 @@ mas sem digitar os nomes das comidas diretamente;
 - Exiba a foodsInfo no console.
 */
 
+const foodsInfo = `Até aqui, o array "brasilianFoods" possui ${brazilianFoods.length} itens: ${brazilianFoods[0]}, ${brazilianFoods[1]} e ${brazilianFoods[2]}`
 
+console.log(foodsInfo)
 
 /*
 06 - Comente o console.log acima e:
@@ -61,7 +69,12 @@ itens "Lilica" e "Matilda";
 - Exiba a "dogNames" no console.
 */
 
+const maleDogNames = ['Chico', 'Zeca']
+const femaleDogNames = ['Lilica', 'Matilda']
 
+let dogNames = maleDogNames.concat(femaleDogNames)
+
+console.log(dogNames)
 
 /*
 07 - Comente o console.log acima e:
@@ -72,7 +85,7 @@ itens "Lilica" e "Matilda";
 - O resultado deve ser: "nome1, nome2, nome3, nome4".
 */
 
-
+console.log(dogNames.join(', '))
 
 /*
 08 - Comente o console.log acima e:
@@ -87,7 +100,13 @@ utilizando o método adequado para isso;
 agora é "Lilica".
 */
 
+console.log(dogNames[dogNames.length-1])
 
+    // dogNames.pop()
+
+    // console.log(dogNames)
+
+    // console.log(dogNames[dogNames.length-1])
 
 /*
 09 - Comente o console.log acima e:
@@ -101,6 +120,12 @@ apenas as letras iniciais dos nomes do array "dogNames";
 - Exiba a "initials" no console.
 */
 
+dogNames.push('Nina')
+console.log(dogNames)
+
+const initials = `${dogNames[0][0]}${dogNames[1][0]}${dogNames[2][0]}${dogNames[3][0]}${dogNames[4][0]}`
+console.log(initials)
+
 
 
 /*
@@ -110,7 +135,7 @@ apenas as letras iniciais dos nomes do array "dogNames";
 - Utilize o método adequado para isso.
 */
 
-
+console.log(initials.toLocaleLowerCase())
 
 /*
 11 - Comente o console.log acima e:
@@ -128,7 +153,11 @@ constante que você criou.
 'A SOBREMESA é um doce à base de coco, tradicional na América Latina e em Angola.'
 */
 
+const dessert = `${dogNames[0].slice(3, 5)}${dogNames[1].slice(2, 4)}${dogNames[3].slice(5,7).replace('d', 'n')}`
 
+console.log(dessert)
+
+console.log(`A ${dessert} é um doce à base de coco, tradicional na America Latina e em Angola`)
 
 /*
 12 - Comente o console.log acima e:
@@ -142,3 +171,10 @@ elevado ao cubo, faça o 2º item do array receber todo o valor que ele
 já tem, mais 4;
 - Agora, o resultado exibido no console deve ser 729.
 */
+
+let oddNumbers = [3, 5, 7]
+oddNumbers[1] += 4
+console.log(oddNumbers[1]**3)
+
+
+
